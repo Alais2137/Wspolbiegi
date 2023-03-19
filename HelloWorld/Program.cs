b2546b33434a87f50a8efd0b0ecf57a34f7d9675
Alais2137 <alicepeacepl@gmail.com>
2023-03-19 18:26:49 +0100

    Pretending it's not only Hello World

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,8 +2,17 @@
 using System;
 namespace HelloNS
 {
+    public class Mathing
+    {
+        public static int Adding(int a, int b)
+        {
+            return a+b;
+        }
+    }
+    
     public class HelloWorld
     {   
+        
         public static void Main()
         {
             Console.WriteLine("Hello, World!");
--- HelloWorldTests/UnitTest1.cs
+++ HelloWorldTests/UnitTest1.cs
@@ -4,13 +4,21 @@ namespace HelloWorldTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void HelloTest()
         {
             var stringWriter = new StringWriter();
             Console.SetOut(stringWriter);
             HelloWorld.Main();
             var output = stringWriter.ToString();
             Assert.AreEqual("Hello, World!\r\n", output);
+        }
+
+        [TestMethod]
+        public void AddingTest()
+        {
+            int a = 4;
+            int b = 3;
+            Assert.AreEqual(Mathing.Adding(a,b), a+b, 7);
         }
     }
 }
